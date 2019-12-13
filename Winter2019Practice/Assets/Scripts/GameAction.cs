183d2c4e59e9b7428d7dca166706e6a1a3ffe505
Kaitlyn <kand8998@gmail.com>
2019-12-13 08:43:27 -0700

    GameAction and Action Handler

---
+++ Winter2019Practice/Assets/Scripts/GameAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class GameAction : ScriptableObject
+{
+    public UnityAction action;
+    public object call;
+    public UnityAction<Transform> transformAction;
+
+    public void Raise()
+    {
+        action?.Invoke();
+    }
+
+    public void Raise(Transform transformObj)
+    {
+        transformAction?.Invoke(transformObj);
+    }
+}

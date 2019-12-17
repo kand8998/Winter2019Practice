a0aba552bffe3394c01ceb883e8d7fa17de13816
Kaitlyn <kand8998@gmail.com>
2019-12-17 09:14:25 -0700

    Character Mover and Int Data

---
+++ Winter2019Practice/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    public float moveSpeed = 10f, jumpSpeed = 30f, gravity = 10f;
+    private CharacterController controller;
+    private Vector3 position;
+    public IntData jumpData;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    private void Update()
+    {
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+        position.z = moveSpeed * Input.GetAxis("Vertical");
+        position.y -= gravity;
+
+        if (Input.GetButtonDown("Jump") && jumpData.value < jumpData.maxValue)
+        {
+            position.y = jumpSpeed;
+            jumpData.value++;
+        }
+        else if (controller.isGrounded)
+        {
+            position.y = 0;
+        }
+        controller.Move(position * Time.deltaTime);
+    }
+}
+

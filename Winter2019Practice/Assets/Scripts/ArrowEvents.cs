fe3ad4fcfd34eaaac68cd0e8b3db457de544c70a
Kaitlyn <kand8998@gmail.com>
2019-12-18 10:13:49 -0700

    Arrow Events Added

---
+++ Winter2019Practice/Assets/Scripts/ArrowEvents.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ArrowEvents : MonoBehaviour
+{
+    public UnityEvent upArrowEvent, downArrowEvent, leftArrowEvent, rightArrowEvent;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            upArrowEvent.Invoke();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            downArrowEvent.Invoke();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            leftArrowEvent.Invoke();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rightArrowEvent.Invoke();
+        }
+    }
+}

6ee9c18875d7ab7fc4118bffee8937bf8a901567
Kaitlyn <kand8998@gmail.com>
2019-12-20 08:18:00 -0700

    Collections Data with Action Call

---
+++ Winter2019Practice/Assets/Scripts/Collections.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Collections : ScriptableObject
+{
+    public List<Object> objectList;
+
+    public List<FloatData> floatDataList;
+
+    public List<GameObject> gameObjectList;
+    
+    public GameAction action;
+    
+
+    public void FindObjectType(Object obj)
+    {
+        foreach (var currentObj in objectList)
+        {
+            if (currentObj == obj)
+            { 
+                action.Raise();
+            }
+        }
+    }
+
+    public void AddToList(Object obj)
+    {
+        objectList.Add(obj);
+    }
+
+    public void RemoveFromList(Object obj)
+    {
+        foreach (var currentObj in objectList)
+        {
+            if (currentObj == obj)
+            {
+                objectList.Remove(obj);
+            }
+        }
+    }
+}

87ce1c35794addc6f1d371c4ee6c66ceb1234b1d
Kaitlyn <kand8998@gmail.com>
2019-12-26 09:57:10 -0700

    Model and Number Guessing Script Added

---
+++ Winter2019Practice/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    //Intial Script Based on a Tutorial
+    private int max = 1000;
+    private int min = 1;
+    private void Start()
+    {
+        Debug.Log("Welcome to number wizard");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+    }
+
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("You hit enter.");
+        }
+    }
+}

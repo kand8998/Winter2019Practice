bb296bfd83aacaca7e0aab1d751e919ff4ea5c51
Kaitlyn <kand8998@gmail.com>
2019-12-27 09:46:06 -0700

    Collection Capacity

---
+++ Winter2019Practice/Assets/Scripts/CollectCapacity.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CollectCapacity : MonoBehaviour
+{
+    public Collections collection;
+    
+    public void CollectionCapacity()
+    {
+        var b = collection.objectList.Capacity == Random.Range(0,10);
+    }
+}

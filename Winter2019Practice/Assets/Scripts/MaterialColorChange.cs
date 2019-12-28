042ddc5d77d2d539456e2bdc5e2074b657ef52ec
Kaitlyn <kand8998@gmail.com>
2019-12-28 12:29:35 -0700

    Material Color Set Script

---
+++ Winter2019Practice/Assets/Scripts/MaterialColorChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialColorChange : MonoBehaviour
+{
+    Color colorStart = Color.black;
+    Color colorEnd = Color.white;
+    float duration = 1.0f;
+    Renderer render;
+    public GameAction colorAction;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+        colorAction.action += ColorFade;
+    }
+
+    private void ColorFade()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        render.material.color = Color.Lerp(colorStart, colorEnd, lerp);
+    }
+}

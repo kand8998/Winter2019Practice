924e0748c2a710f69b16877ec9b1de5cfe1bb2e9
Kaitlyn <kand8998@gmail.com>
2019-12-30 13:32:29 -0700

    bool data with action

---
+++ Winter2019Practice/Assets/Scripts/BoolData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class BoolData : ScriptableObject
+{
+    private bool boolValue;
+    public GameAction trueAction;
+
+    private void SetBoolTrue()
+    {
+        trueAction.action = SetBoolFalse;
+        boolValue = true;
+    }
+
+    private void SetBoolFalse()
+    {
+        boolValue = false;
+        trueAction.Raise();
+    }
+}

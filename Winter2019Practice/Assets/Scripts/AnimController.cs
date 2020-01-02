90df42ebfb88d5b3e03915b06830407f2e30bf81
Kaitlyn <kand8998@gmail.com>
2020-01-02 11:27:42 -0700

    Animator controller

---
+++ Winter2019Practice/Assets/Scripts/AnimController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class AnimController : ScriptableObject
+{
+    public float animSpeed;
+    
+    public void AnimCall(Animator animator)
+    {
+        animator.SetTrigger(name);
+    }
+
+    public void AnimSpeed(Animator animator)
+    {
+        animator.speed = animSpeed;
+    }
+
+    public void AnimEventSet(Animator animator)
+    {
+        animator.fireEvents = true;
+    }
+}

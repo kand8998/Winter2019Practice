3e66a34d035a6f1e7935155e8ae2ebfc924b45e7
Kaitlyn <kand8998@gmail.com>
2020-01-03 09:08:28 -0700

    Ai script added

---
+++ Winter2019Practice/Assets/Scripts/AiWithNavMesh.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AiWithNavMesh : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player, destination;
+    private void Start()
+    {
+        destination = transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        destination = player;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        destination = transform;
+    }
+
+    private void Update()
+    {
+        agent.destination = destination.position;
+        
+    }
+}
